d29b0f323384673f1048fe337ba7ad334ddb2eb5
PaddyGallivan <pgallivan@outlook.com>
2026-05-05 12:55:30 +1000

    fix: send emails from hello@horseracetipping.com

--- index.js
+++ index.js
@@ -140,7 +140,7 @@ async function sendEmail(env, to, subject, html) {
     await fetch("https://api.resend.com/emails", {
       method: "POST",
       headers: { "Authorization": "Bearer " + env.RESEND_API_KEY, "Content-Type": "application/json" },
-      body: JSON.stringify({ from: "Horse Race Tipping <hello@luckdragon.io>", to: [to], subject, html })
+      body: JSON.stringify({ from: "Horse Race Tipping <hello@horseracetipping.com>", to: [to], subject, html })
     });
   } catch {}
 }
